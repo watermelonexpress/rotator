203630901104bb4ef429842b588dcf8119463edd
Gerasimos Athanasopoulos <ath.ger@gmail.com>
2012-06-24 15:16:43 +0300

    Ditched the aws/s3 gem in favor of aws-sdk

--- lib/rotator/uploaders/s3.rb
+++ lib/rotator/uploaders/s3.rb
@@ -1,54 +1,41 @@
-require 'aws/s3'
-
 module Rotator
 
   module Uploaders
   
     class S3 < Base
       # ACCESSORS ---------------------------------------------------------
-      attr_accessor :key, :secret, :bucket
-      attr_reader :s3_options
+      attr_accessor :bucket
+      attr_reader :s3_options, :connection
     
       # METHODS -----------------------------------------------------------
       def initialize(path, opts = {})
-        @include_hostname = opts.delete(:hostname)||true
-        set_s3_options(opts.delete(:s3_options))
-        establish_connection
-        check_connection
+        @hostname = opts.delete(:hostname)
+        self.s3_options = opts.delete(:s3_options)
         super(path, opts)
       end
     
       def upload
         super
         create_bucket
-        response = AWS::S3::S3Object.store(filename, open(path), s3_path)
-        `rm #{path}` if response.code == 200
-        response
-      end
-
-      def establish_connection; AWS::S3::Base.establish_connection!(s3_options); check_connection end
-    
-      def check_connection
-        raise S3ConnectionError.new("Check your configuration settings and try again.") unless AWS::S3::Base.connected?
-      end
-
-      def create_bucket
-        begin
-          AWS::S3::Bucket.find(bucket)
-        rescue AWS::S3::NoSuchBucket
-          AWS::S3::Bucket.create(bucket)
+        connection.buckets[bucket].object[s3_path].write(File.open(path)).tap do |obj|
+          `rm #{path}` if obj.exists?
         end
       end
 
-      def set_s3_options(opts)
-        raise InvalidS3Configuration.new("You must pass options as s3_options: { key: 'your_key', secret: 'your_secret' } ") if opts.blank?
+      def s3_options=(opts)
+        raise InvalidS3Configuration.new(
+          "You must pass options as s3_options: { access_key_id: 'your_key', secret_access_key: 'your_secret' ... } "
+        ) if opts.blank?
         self.bucket = opts.delete(:bucket)||'rotator.files'
         @s3_options = opts
-        self.key = s3_options[:access_key_id]
-        self.secret = s3_options[:secret_access_key]
+        @connection = AWS::S3.new(s3_options)
+      end
+
+      def create_bucket
+        connection.buckets.create(bucket) unless connection.buckets[bucket].exists?
       end
     
-      def s3_path; bucket + (@include_hostname ? ("/" + `hostname`.chomp) : "") end
+      def s3_path; bucket + (@hostname ? ("/" + `hostname`.chomp) : "") end
     end
   
   end
--- rotator.gemspec
+++ rotator.gemspec
@@ -20,6 +20,6 @@ Gem::Specification.new do |s|
 
   # specify any dependencies here; for example:
   s.add_development_dependency "rspec"
-  s.add_development_dependency "aws/s3"
-  # s.add_runtime_dependency "rest-client"
+  s.add_development_dependency "aws-sdk"
+  s.add_runtime_dependency "aws-sdk"
 end
